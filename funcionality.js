2f0e09a5ff618b07cc5b8d23dc04f40e8017cb30
janielson <janielsonsempresantos@hotmail.com>
2023-10-24 15:36:37 -0300

    aplicativo de numero aleatorio pronto

--- funcionality.js
+++ funcionality.js
@@ -1,17 +1,19 @@
+
+// aqui pegamos nosso botão e demos um evento de click para que todo vez que for clicado realise nossa função.
 const myButton = document.querySelector(".button_drawn");
-myButton.addEventListener("click" , myDrawnFunction)
+myButton.addEventListener("click" , myDrawnFunction);
 
 
 
 
 function myDrawnFunction() {
-
+    //  aqui estou sorteando um numero para cima e outro para baixo.
     let inputvalue1 = Math.ceil(document.querySelector(".min").value);
     let inputvalue2 = Math.floor(document.querySelector(".max").value);
    
-
+    // aqui e todo o calculo que e realizado para poder fazer o sorteio
     const numeroAleatorio = Math.floor(Math.random() * (inputvalue2 - inputvalue1 + 1)) + inputvalue1;
-
+    //  aqui colocamos o numero sorteado na tela.
     const output = document.querySelector(".myNumberDrawn");
     output.innerHTML = numeroAleatorio;
   
