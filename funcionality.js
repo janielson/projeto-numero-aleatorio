f9642bc58c0db2fbfe48142748e858c36151c74e
JScript <43323432+janielson@users.noreply.github.com>
2023-10-23 21:58:26 -0300

    vou ajustar o resultado com o css

--- funcionality.js
+++ funcionality.js
@@ -1,5 +1,22 @@
+const myButton = document.querySelector(".button_drawn");
+myButton.addEventListener("click" , myDrawnFunction)
 
 
+
+
+function myDrawnFunction() {
+
+    let inputvalue1 = Math.ceil(document.querySelector(".min").value);
+    let inputvalue2 = Math.floor(document.querySelector(".max").value);
+   
+
+    const numeroAleatorio = Math.floor(Math.random() * (inputvalue2 - inputvalue1 + 1)) + inputvalue1;
+
+    const output = document.querySelector(".myNumberDrawn");
+    output.innerHTML = numeroAleatorio;
+  
+    
+}
 
 
 
